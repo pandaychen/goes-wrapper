a5a43390b769bf99dc99916ff09399c0bf359084
pandaychen <pandaychen@tencent.com>
2022-08-29 21:13:04 +0800

    增加全局错误定义

--- pyerrors/workqueue.go
+++ pyerrors/workqueue.go
@@ -1,5 +1,11 @@
 package pyerrors
 
 var (
-	ErrWorkQueHandlerExists = _addWithMsg(-30001, "redislock: lock not held")
+	ErrWorkQueHandlerExists = _addWithMsg(-30001, "worker handler already registed")
+
+	ErrWorkQueDriverExists = _addWithMsg(-30002, "driver already registed")
+
+	ErrWorkQueBadDriver = _addWithMsg(-30003, "bad driver")
+
+	ErrWorkQueBadDriverConfig = _addWithMsg(-30004, "bad driver config")
 )
